9804a208a5332995f0753143003c48739ab9ab4f
nikhil <nrmeria@gmail.com>
2021-06-05 13:26:10 +0530

    added nsetools in python api file

--- api/date.py
+++ api/date.py
@@ -1,11 +1,16 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
+from nsetools import Nse
+
 
 class handler(BaseHTTPRequestHandler):
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    return
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        nse = Nse()
+        q = nse.get_quote('infy')
+        self.wfile.write(
+            str(q).encode())
+        return
