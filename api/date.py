5bcd7257f009aaefbbc5268c775ac76212b015b0
nikhil <nrmeria@gmail.com>
2021-06-07 15:17:35 +0530

    added requests package in api

--- api/date.py
+++ api/date.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 import json
+import requests
 from nsetools import Nse
 
 
@@ -9,10 +10,13 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        nse = Nse()
-        q = nse.get_quote('infy', as_json=True)
-        data = json.dumps(q)
+        # nse = Nse()
+        # q = nse.get_quote('infy', as_json=True)
+        # data = json.dumps(q)
+
+        res = requests.get("https://jsonplaceholder.typicode.com /todos")
+        data = json.loads(res.text)
 
         self.wfile.write(
-            str(type(data)).encode())
+            str(data).encode())
         return
