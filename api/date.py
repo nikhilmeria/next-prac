96445df5f031b172a0c290421d43ac67d7103064
nikhil <nrmeria@gmail.com>
2021-06-07 12:17:34 +0530

    date api update

--- api/date.py
+++ api/date.py
@@ -6,7 +6,7 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
         nse = Nse()
         q = nse.get_quote('infy')
